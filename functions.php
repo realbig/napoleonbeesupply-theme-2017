51bcd1fdda30e0fd1531fa4478a5e6c096991499
Eric Defore <eric.defore@gmail.com>
2019-01-05 17:13:42 -0500

    v1.0.5

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
  * @since FoundationPress 1.0.0
  */
 
-define( 'THEME_VERSION', '1.0.4' );
+define( 'THEME_VERSION', '1.0.5' );
 
 /** Various clean up functions */
 require_once( 'library/cleanup.php' );
