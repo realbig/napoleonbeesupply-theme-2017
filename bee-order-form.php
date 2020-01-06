0d3b310d088e1782410d50839722430e21566d58
Eric Defore <eric.defore@gmail.com>
2020-01-06 13:37:18 -0500

    Changes to Bee Order Form for 2020

--- bee-order-form.php
+++ bee-order-form.php
@@ -36,10 +36,7 @@ if ( $featured ) {
                     <table id="nbs-bee-order-form">
                         <tr>
                             <th>Item</th>
-                            <th>1-25</th>
-                            <th>26-50</th>
-                            <th>51-99</th>
-                            <th>100+</th>
+                            <th>Price</th>
                             <th>Quantity</th>
                             <th></th>
                         </tr>
@@ -47,41 +44,15 @@ if ( $featured ) {
 						<?php while ( have_posts() ) : ?>
 							<?php the_post(); ?>
 
-							<?php
-							$pricing_data   = false;
-							$pricing_enable = get_post_meta( get_the_ID(), '_as_quantity_range_pricing_enable', true );
-
-							if ( $pricing_enable === 'on' ) {
-
-								$pricing      = get_post_meta( get_the_ID(), '_as_quantity_range_pricing_values', true );
-								$pricing_data = wp_list_pluck( $pricing, 'price', 'min_qty' );
-							}
-							?>
-
                             <tr>
                                 <td>
 									<?php the_title(); ?>
                                 </td>
-
-								<?php
-								$quantities = array( 1, 26, 51, 100 );
-
-								foreach ( $quantities as $quantity ) :
-									?>
-                                    <td>
-										<?php
-										if ( $pricing_data ) {
-
-											echo wc_price( $pricing_data[ $quantity ] );
-
-										} else {
-
-											wc_get_template( 'loop/price.php' );
-										}
-										?>
-                                    </td>
-
-								<?php endforeach; ?>
+                                
+                                <td>
+                                    <?php wc_get_template( 'loop/price.php' ); ?>
+                                </td>
+                                
                                 <td>
                                     <input type="number" name="nbs_bee_order_quantity_<?php the_ID(); ?>"
                                            class="nbs-bee-order-form-qty" value="1"
